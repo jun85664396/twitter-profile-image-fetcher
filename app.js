6dfcec354bb4295884c0d10ea908266f3bdb5d30
JunSangPil <jun85664396@gmail.com>
2017-09-06 23:17:10 +0900

    add redis cache

--- app.js
+++ app.js
@@ -1,23 +1,11 @@
-const request = require('request')
-
 const express = require("express")
+const Profile = require("./profile")
 const app = express()
 
-function get_profile(screen_name, size) {
-  return new Promise((resolve, reject) => {
-    request.get(`https://twitter.com/${screen_name}/profile_image?size=${size}`, function(err, res, body){
-      if(err) return reject(err)
-      if(res.statusCode == 200)
-      {
-        resolve(res.request.uri.href)
-      }
-    }) 
-  })
-}
 app.get("/:screen_name", async function(req, res){
   // mini, normal, bigger, original
   let size = req.query.size || "original"
-  let profile = await get_profile(req.params.screen_name, size)
+  let profile = await Profile.get(req.params.screen_name, size)
   res.redirect(profile)
 })
 
---
+++ profile.js
@@ -0,0 +1,31 @@
+const request = require("request")
+const bluebird = require("bluebird")
+const redis = require("redis"),
+  client = bluebird.promisifyAll(redis.createClient("redis://localhost"))
+
+const profile  = {
+  get_profile: function(screen_name, size) {
+    return new Promise((resolve, reject) => {
+      request.get(`https://twitter.com/${screen_name}/profile_image?size=${size}`, function(err, res, body){
+        if(err) return reject(err)
+        if(res.statusCode == 200)
+        {
+          resolve(res.request.uri.href)
+        }
+      }) 
+    })
+  },
+  get: async function(screen_name, size) {
+    let key = `${screen_name}:${size}`
+    let exists = await client.existsAsync(key)
+    let profile_path
+    if(exists) {
+      profile_path = await client.getAsync(key)
+    }else{
+      profile_path = await this.get_profile(screen_name, size)
+      await client.setexAsync(key, 300, profile_path)
+    }
+    return profile_path
+  }
+}
+module.exports = profile
